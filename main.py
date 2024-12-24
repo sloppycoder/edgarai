ff99b54e07f1f81369f302e33f5d0d22f4966d3a
Li Lin <guru.lin@gmail.com>
2024-12-23 22:00:07 -0800

    proj init

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import json
 import logging
 import logging.config
-import os
-from decimal import Decimal
 from pathlib import Path
+
 from dotenv import load_dotenv
-
-
 
 load_dotenv()
 
@@ -14,6 +11,4 @@ with open(Path(__file__).parent / "logger_config.json", "r") as f:
     logging.config.dictConfig(json.load(f))
 
 if __name__ == "__main__":
-
     print("Hello edgar")
-
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,5 @@
-import asyncio
 import logging
-import os
-
-import pytest
 
 # suppress INFO logs to reduce noise in test output
 root_logger = logging.getLogger()
 root_logger.setLevel(logging.WARN)
-
