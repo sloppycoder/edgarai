69c873af338f5e622bc529a06506e0e9779d7f0f
Li Lin <guru.lin@gmail.com>
2024-12-28 13:07:12 -0800

    print out data that can be used in Python REPL

--- gcp_helper/__main__.py
+++ gcp_helper/__main__.py
@@ -16,7 +16,7 @@ _, project_id = google.auth.default()
 def callback(message):
     # we recieve bytes of a json string
     # we convert it to dict using json.loads then we print it with indentation
-    print(json.dumps(json.loads(message.data.decode("utf-8")), indent=4))
+    print(json.dumps(json.loads(message.data.decode("utf-8")), indent=4) + ",")
     message.ack()
 
 
@@ -33,12 +33,14 @@ def wait_for_response() -> None:
     subscriber = pubsub_v1.SubscriberClient()
     subscription_path = subscriber.subscription_path(project_id, subscription_name)
     streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
-    print(f"Listening for messages on {subscription_path}...")
+    print(f"# Listening for messages on {subscription_path}...")
+    print("[")
 
     try:
         streaming_pull_future.result()
     except KeyboardInterrupt:
-        print("Stopping subscriber...")
+        print("]")
+        print("# Stopping subscriber...")
         streaming_pull_future.cancel()
         streaming_pull_future.result()
 
